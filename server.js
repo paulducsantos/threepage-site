09eaa40047698f2cf4717259f7a48fc56bb4239e
Paul Santos <paulmichael.a.santos@gmail.com>
2016-02-06 14:05:53 -0500

    dashboard can get github repos

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+var GitHubApi = require("node-github");
 var express = require('express');
 var app = express();
 const PORT = process.env.PORT || 8080;
@@ -12,9 +13,24 @@ app.get('/register', function(req, res) {
   res.sendFile(process.cwd() + '/views/register.html');
 });
 
-// app.get('/', function(req, res) {
-//   res.sendFile(process.cwd() + '/views/home.html');
-// });
+app.get('/dashboard', function(req, res) {
+  res.sendFile(process.cwd() + '/views/dashboard.html');
+});
+
+app.get('/dashboard/:githubUsername', function(req, res) {
+  var github = new GitHubApi({
+    version: "3.0.0"
+  });
+
+  github.user.getFrom({
+    user: req.params.githubUsername
+  }, function(err, result) {
+    console.log(result);
+    res.send(JSON.stringify(result));
+  });
+});
+
+
 
 app.listen(PORT, function() {
   console.log('Listening on port %s', PORT);
