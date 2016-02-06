1ac13af8bfc11918515800e0180f925ce7a6b2aa
Paul Santos <paulmichael.a.santos@gmail.com>
2016-02-06 11:15:41 -0500

    added gitignore and created html

--- server.js
+++ server.js
@@ -2,4 +2,20 @@ var express = require('express');
 var app = express();
 const PORT = 8080;
 
-app.use('/static', express.static('public/js'));
+app.use('/static', express.static('public'));
+
+app.get('/', function(req, res) {
+  res.sendFile(process.cwd() + '/views/home.html');
+});
+
+app.get('/register', function(req, res) {
+  res.sendFile(process.cwd() + '/views/register.html');
+});
+
+// app.get('/', function(req, res) {
+//   res.sendFile(process.cwd() + '/views/home.html');
+// });
+
+app.listen(PORT, function() {
+  console.log('Listening on port %s', PORT);
+});
